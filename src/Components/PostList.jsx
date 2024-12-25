19c5636a16e5729dc4506fe8f3b0e0cc7542108f
Adityatitu <Adityatitu01@gmail.com>
2024-12-25 19:00:50 +0530

    starts createsiglePostPage

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -5,6 +5,20 @@ const PostList = () => {
   return (
     <div className='flex flex-col gap-12 mb-8'>
       <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
     </div>
   )
 }
